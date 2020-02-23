7f9516bf83a59111cbf19d5a5c95984f7e8171cc
kidaqrus <adikwusule@gmail.com>
2020-02-23 14:21:44 +0100

    khalifa update

--- khalifasite/settings.py
+++ khalifasite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'site.apps.SiteConfig',
+    'mysite.apps.MysiteConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'khalifasite/static/')
+]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
