b21953e8acdea54b7dfb206ccabc89afbc72ec05
kidaqrus <adikwusule@gmail.com>
2020-02-28 10:57:44 +0100

    changed previous temp to include new template

--- khalifasite/settings.py
+++ khalifasite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mysite.apps.MysiteConfig',
+    'bootstrap4',
+    'fontawesome',
 ]
 
 MIDDLEWARE = [
@@ -117,11 +119,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'khalifasite/static/')
-]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.conf import settings
+
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'mysite/index.html')
